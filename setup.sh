28859d1ecd2713f7b4144394bad37db77a775490
Earlbotay <earltiktokofficial09@gmail.com>
2024-08-05 20:56:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Kemas kini dan pasang kebergantungan sistem
 sudo apt-get update
 sudo apt-get install -y chromium-browser chromium-chromedriver
+pip install selenium requests-html
 
 # Pasang kebergantungan Python
 pip install -r requirements.txt
